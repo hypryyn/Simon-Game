bc11d8c5b42cb8721df0a81ce8cb3b3523f4f1a3
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-04-01 14:20:37 +0700

    adding random selector

--- game.js
+++ game.js
@@ -1,7 +1,19 @@
 let buttonColours = ["red", "blue", "green", "yellow"];
 
+let gamePattern = [];
 
 
 function nextSequence() {
-    let randomNumber = Math.floor(Math.random() * 3);
-}
+
+    let randomNumber = Math.floor(Math.random() * 4);
+  
+    let randomChosenColour = buttonColours[randomNumber];
+  
+    gamePattern.push(randomChosenColour);
+  
+
+    $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    let audio = new Audio("sounds" + randomChosenColour +".mp3");
+    audio.play();
+  }
