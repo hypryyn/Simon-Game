ece863e4412ca1e296253d309c9056fa77b69f98
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-04-02 22:07:16 +0700

    false function check

--- game.js
+++ game.js
@@ -1,15 +1,12 @@
 let buttonColours = ["red", "blue", "green", "yellow"];
 
 let gamePattern = [];
-
-let started = false;
-
-let level = 0;
-
 let userClickedPattern = [];
 
-$(document).keydown(function () {
+let started = false;
+let level = 0;
 
+$(document).keydown(function () {
   if (!started) {
     $("level-title").text("Level" + level);
     nextSequence();
@@ -19,31 +16,33 @@ $(document).keydown(function () {
 })
 
 $(".btn").click(function () {
-
   let userChosenColour = $(this).attr("id");
-
   userClickedPattern.push(userChosenColour);
-
   console.log(userClickedPattern);
-
   playSound(userChosenColour);
   animatePress(userChosenColour);
-
   checkAnswer(userClickedPattern.length - 1);
-
 });
 
 function checkAnswer(currentLevel) {
-  if(gamePattern(currentLevel) === userClickedPattern(currentLevel)){
-  console.log("succes");
-} if (userClickedPattern.length === gamePattern.length) {
+  if (gamePattern(currentLevel) === userClickedPattern(currentLevel)) {
+    console.log("succes");
+  } if (userClickedPattern.length === gamePattern.length) {
     setTimeout(() => {
       nextSequence();
     }, 1000);
-} else {
-  console.log("wrong");
+  } else {
+    console.log("wrong");
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+  }
+
+    startOver();
 }
-}  
 
 
 
@@ -53,7 +52,7 @@ function nextSequence() {
 
   level++;
 
-  $("level-title").text("level" + level);
+  $("#level-title").text("level " + level);
 
   let randomNumber = Math.floor(Math.random() * 4);
 
@@ -78,4 +77,10 @@ function animatePress(currentColor) {
   setTimeout(function () {
     $("#" + currentColor).removeClass("pressed");
   }, 100);
+}
+
+function startOver(){
+  level = 0;
+  gamePattern = [];
+  started = false;
 }
