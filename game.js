05ce1a0df634101180db5583b57cce494649e1f1
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-04-02 22:19:45 +0700

    fixing logic checking function

--- game.js
+++ game.js
@@ -8,40 +8,42 @@ let level = 0;
 
 $(document).keydown(function () {
   if (!started) {
-    $("level-title").text("Level" + level);
+    $("#level-title").text("Level " + level);
     nextSequence();
     started = true;
   }
 
-})
+});
 
 $(".btn").click(function () {
   let userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
-  console.log(userClickedPattern);
+  // console.log(userClickedPattern);
   playSound(userChosenColour);
   animatePress(userChosenColour);
   checkAnswer(userClickedPattern.length - 1);
 });
 
 function checkAnswer(currentLevel) {
-  if (gamePattern(currentLevel) === userClickedPattern(currentLevel)) {
-    console.log("succes");
-  } if (userClickedPattern.length === gamePattern.length) {
-    setTimeout(() => {
-      nextSequence();
-    }, 1000);
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    // console.log("succes");
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(() => {
+        nextSequence();
+      }, 1000);
+    }
   } else {
-    console.log("wrong");
+    // console.log("wrong");
     playSound("wrong");
     $("body").addClass("game-over");
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+
     setTimeout(function () {
       $("body").removeClass("game-over");
     }, 200);
-    $("#level-title").text("Game Over, Press Any Key to Restart");
+    startOver();
   }
 
-    startOver();
 }
 
 
@@ -79,7 +81,7 @@ function animatePress(currentColor) {
   }, 100);
 }
 
-function startOver(){
+function startOver() {
   level = 0;
   gamePattern = [];
   started = false;
